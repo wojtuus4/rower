e16bd84305302efe0cba5ce1a7a717412804232e
unknown <wkapelak@gmail.com>
2018-05-25 09:27:24 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function MojKomponent() {
+  return (
+    <div>
+      <h1>Witaj, świecie!</h1>
+      <h2>Jestem ambitna/y, więc przerabiam kurs React.js!</h2>
+    </div>
+  );
+}
+ReactDOM.render(
+  <MojKomponent />,
+  document.getElementById('app')
+);
